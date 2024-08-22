4d3cc970731333fb584eb93c3f21feae0de2b403
Max Inden <mail@max-inden.de>
2024-08-22 14:30:28 +0200

    debug instead of trace

--- .github/workflows/rust-android-run-tests-on-emulator.sh
+++ .github/workflows/rust-android-run-tests-on-emulator.sh
@@ -7,7 +7,7 @@ while [[ -z "$(adb shell getprop sys.boot_completed | tr -d '\r')" ]]; do sleep 
 any_failures=0
 for test in $(find target/$TARGET/debug/deps/ -type f -executable ! -name "*.so" -name "*-*"); do
     adb push "$test" /data/local/tmp/
-    adb shell RUST_LOG=trace /data/local/tmp/$(basename "$test") --nocapture || any_failures=1
+    adb shell RUST_LOG=debug /data/local/tmp/$(basename "$test") --nocapture || any_failures=1
 done
 
 exit $any_failures
