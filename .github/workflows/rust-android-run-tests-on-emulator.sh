6609cae14f193a756087b2aa743a5d8b1aab0a10
Max Inden <mail@max-inden.de>
2024-08-22 13:34:06 +0200

    no chmod

--- .github/workflows/rust-android-run-tests-on-emulator.sh
+++ .github/workflows/rust-android-run-tests-on-emulator.sh
@@ -7,7 +7,6 @@ while [[ -z "$(adb shell getprop sys.boot_completed | tr -d '\r')" ]]; do sleep 
 any_failures=0
 for test in $(find target/$TARGET/debug/deps/ -type f -executable ! -name "*.so" -name "*-*"); do
     adb push "$test" /data/local/tmp/
-    adb shell chmod +x /data/local/tmp/$(basename "$test")
     adb shell /data/local/tmp/$(basename "$test") || any_failures=1
 done
 
