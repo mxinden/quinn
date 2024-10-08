c69940e4c8c0eb3874d86a4cff1d388d5237d409
Lars Eggert <lars@eggert.org>
2024-10-08 17:44:44 +0300

    Update quinn-udp/src/unix.rs
    Co-authored-by: Max Inden <mail@max-inden.de>

--- quinn-udp/src/unix.rs
+++ quinn-udp/src/unix.rs
@@ -358,7 +358,8 @@ fn send(state: &UdpSocketState, io: SockRef<'_>, transmit: &Transmit<'_>) -> io:
     let addr = socket2::SockAddr::from(transmit.destination);
     let segment_size = transmit.segment_size.unwrap_or(transmit.contents.len());
     let mut cnt = 0;
-    for (i, chunk) in transmit.contents.chunks(segment_size).enumerate() {
+    debug_assert!(transmit.contents.div_ceil(segment_size) <= BATCH_SIZE);
+    for (i, chunk) in transmit.contents.chunks(segment_size).enumerate().take(BATCH_SIZE) {
         prepare_msg(
             &Transmit {
                 destination: transmit.destination,
